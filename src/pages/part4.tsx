19c6d970afc3e365c8fc807994ed8e4b0d2fd184
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2024-08-15 14:29:30 +0800

    placeholder getServerSideProps

--- src/pages/part4.tsx
+++ src/pages/part4.tsx
@@ -1,5 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   CartesianGrid,
   Legend,
@@ -26,40 +27,40 @@ interface Data {
   supBidQse: number;
   supPrice: number;
 }
-async function getData() {
-  const monthData: Data[][] = [];
-  for (let month = 3; month <= 5; month++) {
-    const url = `http://localhost:3001/data/settle-value?startDate=${dayjs()
-      .month(month - 1)
-      .startOf("month")
-      .format("YYYY-MM-DD")}&endDate=${dayjs()
-      .month(month - 1)
-      .endOf("month")
-      .format("YYYY-MM-DD")}`;
-    const response = await fetch(url);
-    const data = (await response.json()) as Data[];
-    monthData.push(data);
+async function getData(month: number = 3) {
+  if (month === 4) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
   }
-  return monthData;
+  const url = `http://localhost:3001/data/settle-value?startDate=${dayjs()
+    .month(month - 1)
+    .startOf("month")
+    .format("YYYY-MM-DD")}&endDate=${dayjs()
+    .month(month - 1)
+    .endOf("month")
+    .format("YYYY-MM-DD")}`;
+  const response = await fetch(url);
+  const data = (await response.json()) as Data[];
+  return data;
 }
 interface Part4Props {
-  monthData: Data[][];
+  marchData: Data[];
 }
 
 export async function getServerSideProps() {
   const data = await getData();
   return {
-    props: { monthData:data },
-  } as { props: Part4Props};
+    props: { marchData: data },
+  } as { props: Part4Props };
 }
 
 function Part4(props: Part4Props) {
+  console.log(props);
   const [dataMonth, setDataMonth] = useState(3);
-  const [showData, setShowData] = useState<Data[]>([]);
-  useEffect(() => {
-    setShowData(props.monthData[dataMonth-3]);
-    return () => {};
-  }, [dataMonth]);
+  const { data, status, error } = useQuery({
+    queryFn: () => getData(dataMonth),
+    queryKey: [dataMonth],
+    placeholderData: props.marchData,
+  });
   return (
     <div className="w-2/5 bg-secondary p-8 rounded-lg mt-6 text-center">
       <h1>{dataMonth}月的資料</h1>
@@ -67,14 +68,21 @@ function Part4(props: Part4Props) {
       <button onClick={() => setDataMonth(4)}>4月</button>
       <button onClick={() => setDataMonth(5)}>5月</button>
       {(function () {
+        if (status === "error") {
+          return "Error: " + error;
+        }
+        if (status === "pending") {
+          return "Loading...";
+        }
+
         // Empty state
-        if (showData.length === 0) {
+        if (data.length === 0) {
           return "No data";
         }
         return (
           <ResponsiveContainer width="100%" height={400}>
             <LineChart
-              data={showData}
+              data={data}
               margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
